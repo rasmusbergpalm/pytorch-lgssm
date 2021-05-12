fe2fc52d58265e5334884dfa47e0a5542f34a029
rasmusbergpalm <rasmusbergpalm@gmail.com>
2021-05-12 15:34:05 +0200

    Initial release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='lgssm',
+      version='0.0.1',
+      description='A minimal Linear Gaussian State Space Model for pytorch.',
+      url='git@github.com:rasmusbergpalm/pytorch-lgssm.git',
+      maintainer='Rasmus Berg Palm',
+      maintainer_email='rasmusbergpalm@gmail.com',
+      license='MIT',
+      packages=['lgssm'],
+      zip_safe=False,
+      install_requires=[
+          'torch>=1.4.0'
+      ])
